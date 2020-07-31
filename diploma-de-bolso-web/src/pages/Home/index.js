bc65f4a38dc10cd571477c6a8b042a27ebfa3ad7
ConradoLuiz <conradol.vargas@gmail.com>
2020-07-31 20:53:47 -0300

    added wheel section to home page

--- diploma-de-bolso-web/src/pages/Home/index.js
+++ diploma-de-bolso-web/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Classnames from "classnames";
 import styles from "./home.module.scss";
 import OnlineReading from "../../assets/undraw_online_reading_np7n.svg";
+import WheelBlob from "../../assets/undraw-with-blob.svg";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { useDesktop } from "../../utils/mediaQueries";
@@ -12,6 +13,7 @@ export function Home() {
   return (
     <div className={styles.homeContainer}>
       <Header />
+
       <main className={Classnames(!isDesktop ? styles.mainMobile : undefined)}>
         <div
           className={Classnames(
@@ -36,6 +38,27 @@ export function Home() {
           <img src={OnlineReading} alt="Leitura online" />
         </div>
       </main>
+
+      <div
+        className={Classnames(
+          styles.wheelContainer,
+          !isDesktop ? styles.wheelContainerMobile : undefined
+        )}
+      >
+        <img src={WheelBlob} alt="Cadeira de rodas" />
+
+        <div className={styles.wheelText}>
+          <h2>
+            Com a nossa plataforma web, todos tem uma chance de adquirir
+            conhecimento
+          </h2>
+          <h3>
+            Através de um <strong>computador pessoal</strong>,{" "}
+            <strong>celular de um amigo</strong>, ou até de uma{" "}
+            <strong>biblioteca pública</strong>
+          </h3>
+        </div>
+      </div>
       <Footer />
     </div>
   );
