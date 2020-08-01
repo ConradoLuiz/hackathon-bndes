b26396da031e7316aa2c863a2002ed2dfc7eddb7
ConradoLuiz <conradol.vargas@gmail.com>
2020-07-31 22:20:45 -0300

    initial commit for api

---
+++ diploma-de-bolso-api/src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bem vindo a API do Diploma de Bolso",
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error("Not Found - " + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
